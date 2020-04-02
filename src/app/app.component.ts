7c9771bbd2a8fc3db2f5b42990cce7664206ab4d
BadariNarayana Mamidi <badarinarayana.mamidi@lntinfotech.com>
2020-04-02 22:56:31 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient ,HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, Inject, Injectable } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exchangeCurrencyApp';
-}
+  constructor(private http: HttpClient) { }
+  //users: Todo[];
+   restItemsUrl = "http://localhost:8080/exchangeService/currentRate";
+ headers:any;
+    result:any;
+    currentExchangeCurrency = false;
+    historyExchangeCurrency=false;
+    historyExchangeCurrencyResult=[];
+  ngOnInit() {
+    
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa("admin" + ':' + "password") });
+
+    this.http.get(this.restItemsUrl,{headers}).subscribe( (data: any) => {
+      if(data!=null){
+        this.result=data.rates;
+      this.currentExchangeCurrency=true;
+
+       
+ }});
+    this.historyCurrency();
+
+    }
+
+    historyCurrency(){
+      let historyCurrencyUrl = "http://localhost:8080/exchangeService/historyRates";
+
+      this.http.get(historyCurrencyUrl).subscribe( (data: any) => {
+        if(data!=null){
+          this.historyExchangeCurrencyResult=data.rates;
+          this.historyExchangeCurrency=true;
+          this.currentExchangeCurrency=false;
+        }});
+  
+
+    }
+  }
---
+++ src/app/hisotryExchange.ts
@@ -0,0 +1,5 @@
+export class historyExchange {
+    noteId:  number ; 
+    rates:  String[] ;
+  }
+  
